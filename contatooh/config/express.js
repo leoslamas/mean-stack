3761347d848e4c9508755f1febb4d07e5cc875ee
Leonardo Lamas <leoslamas@gmail.com>
2017-04-18 02:51:56 -0300

    Environment variables and middlewares

--- contatooh/config/express.js
+++ contatooh/config/express.js
@@ -2,7 +2,12 @@ var express = require('express');
 
 module.exports = function(){
 	var app = express();
+	
+	//port
 	app.set('port', 3000);
+	
+	//middleware
+	app.use(express.static('./public'));
 
 	return app;
 };
