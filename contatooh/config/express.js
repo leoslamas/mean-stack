892a69c3d9ea02478965449e998c7337c82317f3
Leonardo Lamas <leoslamas@gmail.com>
2017-04-18 03:06:29 -0300

    Configuring routes

--- contatooh/config/express.js
+++ contatooh/config/express.js
@@ -1,15 +1,19 @@
 var express = require('express');
+var home = require('../app/routes/home');
 
 module.exports = function(){
 	var app = express();
-	
-	//port
+
+	//vars
 	app.set('port', 3000);
-	
-	//middleware
-	app.use(express.static('./public'));
 	app.set('view engine', 'ejs');
 	app.set('views', './app/views');
+
+	//middleware
+	app.use(express.static('./public'));
+	app.use(app.router);
+
+	home(app);
 
 	return app;
 };
