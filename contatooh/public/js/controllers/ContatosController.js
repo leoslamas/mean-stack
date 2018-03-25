5ef1d4f76e552000a45c2bc90a85762654272aa1
Leonardo Lamas <leoslamas@gmail.com>
2018-03-25 11:27:00 -0300

    Obtaining contacts with '$'http

--- contatooh/public/js/controllers/ContatosController.js
+++ contatooh/public/js/controllers/ContatosController.js
@@ -1,39 +1,13 @@
-angular.module('contatooh').controller('ContatosController', function($scope){
-    $scope.total = 0; 
+angular.module('contatooh').controller('ContatosController', function ($scope, $http) {
+        $scope.contatos = [];
+        $scope.filtro = '';
 
-    $scope.incrementa = function(){
-        $scope.total++;
-    }
-
-    //	Object
-    $scope.contato	=	{
-        nome:	'Flávio	Henrique',	
-        sobrenome:	'Souza	Almeida'
-    };
-    //	boolean
-    $scope.exibir	=	true;
-    //	Number
-    $scope.salario	=	100.12;
-    //	Date
-    $scope.admissao	=	new	Date();
-
-    $scope.contatos = [
-        {
-            "_id": 1,
-            "nome": "Contato Angular 1",
-            "email": "cont1@empresa.com.br"
-        },
-        {
-            "_id": 2,
-            "nome": "Contato Angular 2",
-            "email": "cont2@empresa.com.br"
-        },
-        {
-            "_id": 3,
-            "nome": "Contato Angular 3",
-            "email": "cont3@empresa.com.br"
-        }
-    ];
-
-    $scope.filtro = '';
+        $http.get('/contatos')
+            .success(function (data) {
+                $scope.contatos = data;
+            })
+            .error(function (statusText) {
+                console.log("Não foi possível obter a lista de contatos");
+                console.log(statusText);
+        });
 });
