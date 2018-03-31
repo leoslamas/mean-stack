1975c52cde70504fdda7302b5032d24c0aa5c697
Leonardo Lamas <leoslamas@gmail.com>
2018-03-30 22:29:37 -0300

    Showing selected contact

--- contatooh/public/js/controllers/ContatoController.js
+++ contatooh/public/js/controllers/ContatoController.js
@@ -1,5 +1,20 @@
-angular.module('contatooh').controller('ContatoController', function($scope, $routeParams) {
-    
+angular.module('contatooh').controller('ContatoController', function($scope, $routeParams, $resource) {
     console.log($routeParams.contatoId);
+
+    var Contato = $resource('/contatos/:id');
+    if ($routeParams.contatoId) {
+        Contato.get({ id: $routeParams.contatoId },
+            function (contato) {
+                $scope.contato = contato;
+            },
+            function (erro) {
+                $scope.mensagem = {
+                    texto: 'Contato não existe. Novo contato.'
+                };
+            }
+        );
+    } else {
+        $scope.contato = {};
+    }
     
 });
