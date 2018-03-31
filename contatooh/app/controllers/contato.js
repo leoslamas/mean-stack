41f96af128d9f76f5cd16b3c8ccb7dba1ff2bea2
Leonardo Lamas <leoslamas@gmail.com>
2018-03-31 00:09:59 -0300

    Adding [create] route to Express

--- contatooh/app/controllers/contato.js
+++ contatooh/app/controllers/contato.js
@@ -5,6 +5,7 @@ var contatos = [
 ];
 module.exports = function() {
 	var controller = {};
+	var ID_CONTATO_INC = 3;
 	
 	controller.listaContatos = function(req, res) {
 	// envia a lista
@@ -28,6 +29,30 @@ module.exports = function() {
 		});
 		res.status(204).end();
 	};
+
+	controller.salvaContato = function (req, res) {
+		var contato = req.body;
+		contato = contato._id ?
+			atualiza(contato) :
+			adiciona(contato);
+		res.json(contato);
+	};
+
+	function adiciona(contatoNovo) {
+		contatoNovo._id = ++ID_CONTATO_INC;
+		contatos.push(contatoNovo);
+		return contatoNovo;
+	}
+
+	function atualiza(contatoAlterar) {
+		contatos = contatos.map(function (contato) {
+			if (contato._id == contatoAlterar._id) {
+				contato = contatoAlterar;
+			}
+			return contato;
+		});
+		return contatoAlterar;
+	}
 
 	return controller;
 }
