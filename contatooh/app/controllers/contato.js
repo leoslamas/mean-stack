c09e2ecbcc3a3bac647f276e60517e06ed3ddf35
Leonardo Lamas <leoslamas@gmail.com>
2018-04-06 04:48:01 -0300

    Find document by ID

--- contatooh/app/controllers/contato.js
+++ contatooh/app/controllers/contato.js
@@ -16,7 +16,17 @@ module.exports = function(app) {
 	};
 
 	controller.obtemContato = function(req, res) {
-		
+		var _id = req.params.id;
+		Contato.findById(_id).exec()
+			.then(function (contato) {
+				if (!contato) throw new Error("Contato não encontrado");
+				res.json(contato);
+			},
+				function (erro) {
+					console.log(erro);
+					res.status(404).json(erro);
+				}
+			);
 	};
 
 	controller.removeContato = function(req, res) {
