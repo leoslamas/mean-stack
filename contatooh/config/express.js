ff00824f2a907cffe314512f119599988f7801c2
Leonardo Lamas <leoslamas@gmail.com>
2018-04-20 10:39:38 -0300

    OAuth 2.0 with Passport

--- contatooh/config/express.js
+++ contatooh/config/express.js
@@ -1,6 +1,9 @@
+var express = require('express');
 var load = require('express-load');
 var bodyParser = require('body-parser');
-var express = require('express');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var passport = require('passport');
 
 module.exports = function(){
 	var app = express();
@@ -15,6 +18,17 @@ module.exports = function(){
 	app.use(bodyParser.urlencoded({extended: true}));
 	app.use(bodyParser.json());
 	app.use(require('method-override')());
+	app.use(cookieParser());
+	app.use(session(
+		{
+			secret: 'homem avestruz',
+			resave: true,
+			saveUninitialized: true
+		}
+	));
+	app.use(passport.initialize());
+	app.use(passport.session());
+	
 	// deprecated!
 	// app.use(app.router);
 	load('models', {cwd: 'app'})
