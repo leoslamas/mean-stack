74f8a970a27de3feacd6e59c980b32c9ecb23ee2
Leonardo Lamas <leoslamas@gmail.com>
2018-04-26 07:40:42 -0300

    Protecting resources

--- contatooh/server.js
+++ contatooh/server.js
@@ -1,6 +1,8 @@
 var http = require('http');
+var express = require('express');
 var app = require('./config/express')();
-require('./config/database.js')('mongodb://localhost/contatooh');
+require('./config/passport')();
+require('./config/database')('mongodb://localhost/contatooh');
 
 http.createServer(app).listen(app.get('port'), function(){
 	console.log('Express Server escutando na porta ' + app.get('port'));
