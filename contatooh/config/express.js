e87eac9381e1d01a30ce4878301be4077d746988
Leonardo Lamas <leoslamas@gmail.com>
2018-07-22 03:15:44 -0300

    Helmet: security middlewares

--- contatooh/config/express.js
+++ contatooh/config/express.js
@@ -4,10 +4,11 @@ var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var passport = require('passport');
+var helmet = require('helmet');
 
 module.exports = function(){
 	var app = express();
-
+	
 	//vars
 	app.set('port', 3000);
 	app.set('view engine', 'ejs');
@@ -28,7 +29,13 @@ module.exports = function(){
 	));
 	app.use(passport.initialize());
 	app.use(passport.session());
-	
+	app.use(helmet());
+	//app.disable('x-powered-by');
+	app.use(helmet.hidePoweredBy({ setTo: 'PHP 5.5.14' }));
+	app.use(helmet.xframe());
+	app.use(helmet.xssFilter());
+	app.use(helmet.nosniff());
+
 	// deprecated!
 	// app.use(app.router);
 	load('models', {cwd: 'app'})
