c73a8d0927dd528942bf7a17698f73a43e86b69b
Leonardo Lamas <leoslamas@gmail.com>
2018-07-24 01:45:09 -0300

    Tasks

--- contatooh/Gruntfile.js
+++ contatooh/Gruntfile.js
@@ -1,4 +1,15 @@
 module.exports = function (grunt) {
+    grunt.initConfig({
+        tarefa1: {
+            /* configurações da tarefa1 */
+        },
+        tarefa2: {
+            /* configurações do tarefa2 */
+        },
+        copy: {
+            /* configurações da tarefa grunt-contrib-copy */
+        }
+    });
+    
     grunt.loadNpmTasks('grunt-contrib-copy');
-
 };
