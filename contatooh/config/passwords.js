45c7c0296d0c8c7ea047f5a13ec91b6da84b3681
leonardo_lamas <leonardo.lamas@bemobi.com.br>
2018-07-30 19:35:38 -0300

    creating test login

---
+++ contatooh/config/passwords.js
@@ -0,0 +1,11 @@
+var l = function(){
+    this.login = function(){
+        return "leonardo.lamas@bemobi.com.br";
+    }
+
+    this.pass = function(){
+        return "TestPassLeoS2018";
+    }
+}
+
+module.exports = l;
