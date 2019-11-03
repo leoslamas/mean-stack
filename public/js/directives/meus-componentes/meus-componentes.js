25dbd7719918f2968e690a257efc6267c7fca1e8
Leonardo Lamas <leoslamas@gmail.com>
2019-11-03 17:09:14 -0300

    More directives (button)

--- public/js/directives/meus-componentes/meus-componentes.js
+++ public/js/directives/meus-componentes/meus-componentes.js
@@ -13,4 +13,21 @@ angular.module('meusComponentes', [])
         directive.templateUrl = 'js/directives/meus-componentes/meu-painel.html';
 
         return directive;
+    })
+    .directive('meuBotaoAviso', function () {
+        var directive = {};
+
+        directive.restrict = "E";
+
+        directive.scope = {
+            nome: '@',
+            acao: '&'
+        };
+
+        directive.template = '<button ng-click="acao()" class="btn btn-warning">'
+            + '{{nome}}'
+            + '</button>';
+
+
+        return directive;
     });
