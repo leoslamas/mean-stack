f78fce21e95bf472b446636ffa7e1e482013dd23
Leonardo Lamas <leoslamas@gmail.com>
2019-11-03 16:25:09 -0300

    Isolating directive scope

--- public/js/directives/meus-componentes/meus-componentes.js
+++ public/js/directives/meus-componentes/meus-componentes.js
@@ -4,6 +4,10 @@ angular.module('meusComponentes', [])
 
         directive.restrict = "EA";
 
+        directive.scope = {
+            titulo: '@' //valor igual a chave 'titulo' pode ser omitido
+        };
+
         directive.template =
         '<div class="panel panel-default">' +
         '  <div class="panel-heading">' +
