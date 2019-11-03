c1823f34a020154b09bc188d2739908774432ba2
Leonardo Lamas <leoslamas@gmail.com>
2019-11-03 16:59:10 -0300

    Splitting DDO from its template

--- public/js/directives/meus-componentes/meus-componentes.js
+++ public/js/directives/meus-componentes/meus-componentes.js
@@ -10,14 +10,7 @@ angular.module('meusComponentes', [])
 
         directive.transclude = true;
 
-        directive.template =
-        '<div class="panel panel-default">' +
-        '  <div class="panel-heading">' +
-        '    <h3 class="panel-title">{{titulo}}</h3>' +
-        ' </div>' +
-        '  <div ng-transclude class="panel-body">' +
-        ' </div>' +
-        '</div>';
+        directive.templateUrl = 'js/directives/meus-componentes/meu-painel.html';
 
         return directive;
     });
