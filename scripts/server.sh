15512040434d47e940ee5056be7006217e7b4905
Leonardo Lamas <leoslamas@gmail.com>
2019-11-04 02:10:16 -0300

    Preparing for deploy

--- scripts/server.sh
+++ scripts/server.sh
@@ -3,3 +3,4 @@ export SELENIUM_USER = nome_de_um_usuario_no_github
 export SELENIUM_USER_PASSWORD = senha_do_usuario
 export CLIENT_ID = ***REMOVED***
 export CLIENT_SECRET = ***REMOVED***
+node server
